1972df51b0661395a6d6250295c9ca80afa4c42b
agusgarrido <garriagus@gmail.com>
2026-06-29 17:20:01 -0300

    Fix: Rutas nuevas.

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { useState } from "react";
 import MainLayout from "../components/layout/MainLayout";
 import { MenuPage } from "../pages/MenuPage";
+import { GamePage } from "../pages/GamePage";
 
 export function AppRouter() {
   const [playerName, setPlayerName] = useState("Anónimo");
@@ -16,6 +17,16 @@ export function AppRouter() {
               <MenuPage playerName={playerName} onNameChange={setPlayerName} />
             }
           />
+        </Route>
+
+        <Route element={<MainLayout showNavbar />}>
+          <Route
+            path="/game"
+            element={<GamePage playerName={playerName} onGameEnd={() => {}} />}
+          />
+          <Route path="/result" element={<div>Result</div>} />
+          <Route path="/instructions" element={<div>Instructions</div>} />
+          <Route path="/leaderboard" element={<div>Leaderboard</div>} />
         </Route>
       </Routes>
     </BrowserRouter>
