45d31f88f1ffce109835e8f460af8081af40f8a8
agusgarrido <garriagus@gmail.com>
2026-06-29 18:24:01 -0300

    Fix: Foco automático sobre el input

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "./Navbar";
+import { LetterBackground } from "../ui/LetterBackground";
 
 interface MainLayoutProps {
   showNavbar?: boolean;
@@ -8,10 +9,11 @@ interface MainLayoutProps {
 const MainLayout = ({ showNavbar = true }: MainLayoutProps) => {
   return (
     <div className="h-dvh flex flex-col bg-navy relative">
+      <LetterBackground/>
       <div className="z-1 flex flex-col flex-1 overflow-hidden">
         {showNavbar && <Navbar />}
         <div className="flex-1 flex items-center justify-center p-6">
-          <div className="w-full max-w-5xl flex flex-col overflow-hidden rounded-2xl border border-border bg-navy2 max-h-[85dvh]">
+          <div className="w-full max-w-5xl flex flex-col overflow-hidden rounded-2xl border border-border bg-navy2 h-[85dvh]">
             <Outlet />
           </div>
         </div>
